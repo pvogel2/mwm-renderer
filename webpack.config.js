52c0b94b350a93cfbbaeed12463c3288fd61c97f
pvogel2 <pvogel2@freenet.de>
2020-05-01 11:11:01 +0200

    format

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: {main: './src/js/renderer.js',test: './dev/js/test.js'},
+  entry: {
+    main: './src/js/renderer.js',
+    test: './src/js/test.js'
+  },
   devServer: {
     contentBase: '.'
   },
