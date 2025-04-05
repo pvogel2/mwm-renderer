44f73ff9612ad2ac2eba24f92718b702588bff2c
Peter Vogel <pvogel2@freenet.de>
2025-04-05 09:26:03 +0200

    refactor: update project

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,15 @@ import pkg from './package.json';
 export default [
 	{
 		input: 'src/js/renderer.js',
+		external: ['three'],
 		output: {
+			globals: {
+				three: 'THREE',
+			},
 			name: 'mwm',
 			file: pkg.browser,
 			format: 'umd'
 		},
-		external: ['three'],
 		plugins: [
 			nodeResolve(), // so Rollup can find `ms`
 			commonjs() // so Rollup can convert `ms` to an ES module
